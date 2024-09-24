d70ba6f7b3910881d42fb565c3b8cf5ddf4c7556
Antônio <antoniomarcos.souza2002@gmail.com>
2024-09-24 14:38:52 -0300

    feat: configurando os presets de qualidade de vídeo e áudio

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 ffmpeg -f v4l2 -video_size 1920x1080 -framerate 30 -i /dev/video0 \
     -f alsa -ac 2 -i hw:1 \
-    -c:v libx265 -c:a aac \
+    -c:v libx265 -preset medium -crf 22 \
+    -c:a aac -b:a 128k \
     -af aresample=async=1 \
-    -t 00:00:10 output.mp4 -y
+    -t 00:00:10 00_H265-recorded.mp4 -y
+
