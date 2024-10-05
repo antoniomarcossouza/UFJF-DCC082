c4b4bf5edaa15ac4ae7659a5f69eb8a6435f282f
Antônio <antoniomarcos.souza2002@gmail.com>
2024-10-05 17:33:03 -0300

    feat: streaming up funcional

--- rtp_streaming.sh
+++ rtp_streaming.sh
@@ -1,1 +1,2 @@
-ffmpeg -re -i ./04/ts_container.ts -c:v libx264 -c:a aac -f rtp_mpegts rtp://localhost:5004
+# ffplay -protocol_whitelist rtp,udp -i "udp://localhost:5004"
+ffmpeg -re -i ./04_TS/ts_container.ts -c:v libx264 -c:a aac -f rtp_mpegts rtp://localhost:5004
